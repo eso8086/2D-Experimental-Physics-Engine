bc35eb117f323d70107ac2951627319eeaeed8c8
eso8086 <elicodingyt@gmail.com>
2024-12-22 19:54:36 +0400

    comments removed

--- src/Physics/Vec2.h
+++ src/Physics/Vec2.h
@@ -10,35 +10,35 @@ struct Vec2 {
     
     ~Vec2() = default;
 
-    void Add(const Vec2& v);                 // v1.Add(v2)
-    void Sub(const Vec2& v);                 // v1.Sub(v2)
-    void Scale(const float n);               // v1.Scale(n)
-    Vec2 Rotate(const float angle) const;    // v1.Rotate(angle)
+    void Add(const Vec2& v);
+    void Sub(const Vec2& v);
+    void Scale(const float n);
+    Vec2 Rotate(const float angle) const;
 
-    float Magnitude() const;                 // v1.Magnitude()
-    float MagnitudeSquared() const;          // v1.MagnitudeSquared()
+    float Magnitude() const;
+    float MagnitudeSquared() const;
 
-    Vec2& Normalize();                       // v1.Normalize()
-    Vec2 UnitVector() const;                 // v1.UnitVector()
-    Vec2 Normal() const;                     // n = v1.Normal()
+    Vec2& Normalize();
+    Vec2 UnitVector() const;
+    Vec2 Normal() const;
 
-    float Dot(const Vec2& v) const;          // v1.Dot(v2)
-    float Cross(const Vec2& v) const;        // v1.Cross(v2)
+    float Dot(const Vec2& v) const;
+    float Cross(const Vec2& v) const;
 
-    Vec2& operator = (const Vec2& v);        // v1 = v2
-    bool operator == (const Vec2& v) const;  // v1 == v2
-    bool operator != (const Vec2& v) const;  // v1 != v2
+    Vec2& operator = (const Vec2& v);
+    bool operator == (const Vec2& v) const;
+    bool operator != (const Vec2& v) const;
     
-    Vec2 operator + (const Vec2& v) const;   // v1 + v2
-    Vec2 operator - (const Vec2& v) const;   // v1 - v2
-    Vec2 operator * (const float n) const;   // v1 * n
-    Vec2 operator / (const float n) const;   // v1 / n
-    Vec2 operator - ();                      // -v1
+    Vec2 operator + (const Vec2& v) const;
+    Vec2 operator - (const Vec2& v) const;
+    Vec2 operator * (const float n) const;
+    Vec2 operator / (const float n) const;
+    Vec2 operator - ();
 
-    Vec2& operator += (const Vec2& v);       // v1 += v2
-    Vec2& operator -= (const Vec2& v);       // v1 -= v2
-    Vec2& operator *= (const float n);       // v1 *= n
-    Vec2& operator /= (const float n);       // v1 /= n
+    Vec2& operator += (const Vec2& v);
+    Vec2& operator -= (const Vec2& v);
+    Vec2& operator *= (const float n);
+    Vec2& operator /= (const float n);
 };
 
 #endif
